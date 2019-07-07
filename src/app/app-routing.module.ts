e18ae3add141ad82cb42807d44c8964d349b3f32
Trym Lund Flogard <trym@flogard.local>
2019-07-07 15:42:02 +0200

    lazy loading of dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then( mod => mod.DashboardModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
