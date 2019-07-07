83043c2e5c8ac4140a2182ccf3b5f50abde0c61e
Trym Lund Flogard <trym@flogard.local>
2019-07-07 16:56:51 +0200

    minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then( mod => mod.DashboardModule) }
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then( mod => mod.DashboardModule) },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
 ];
 
 @NgModule({
