a8aadcc98de079a2d039e9cb66763d4de16c9ef7
Trym Lund Flogard <trym@flogard.local>
2019-07-08 17:49:51 +0200

    eventemitter + length

--- src/app/dashboard/shared/inline-edit/inline-edit.component.ts
+++ src/app/dashboard/shared/inline-edit/inline-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Optional, Host } from '@angular/core';
+import { Component, Input, Optional, Host, Output, EventEmitter } from '@angular/core';
 import { SatPopover } from '@ncstate/sat-popover';
 import { filter } from 'rxjs/operators';
 
@@ -8,15 +8,16 @@ import { filter } from 'rxjs/operators';
   template: `
     <form (ngSubmit)="onSubmit()">
       <div class="mat-subheading-2">Edit value</div>
-      
+
       <mat-form-field>
-        <input matInput maxLength="140" name="comment" [(ngModel)]="comment">
-        <mat-hint align="end">{{comment?.length || 0}}/140</mat-hint>
+        <input matInput [maxLength]="inputLength" name="comment" [(ngModel)]="comment">
+        <mat-hint align="end">{{comment?.length || 0}}/{{ inputLength }}</mat-hint>
       </mat-form-field>
 
       <div class="actions">
         <button mat-button type="button" color="primary" (click)="onCancel()">CANCEL</button>
-        <button mat-button type="submit" color="primary">SAVE</button>
+        <!-- Disables save button if value hasn't changed or if comment length is 0 -->
+        <button mat-button type="submit" color="primary" [disabled]="!comment.length || this._value === this.comment">SAVE</button>
       </div>
     </form>
   `
@@ -29,7 +30,16 @@ export class InlineEditComponent {
   set value(x: string) {
     this.comment = this._value = x;
   }
-  private _value = '';
+  @Input()
+  get length() { return this.inputLength }
+  set length(x: number) { this.inputLength = x }
+
+  @Output() valueChange = new EventEmitter()
+
+
+
+  _value = ''; // original value of field
+  inputLength: number // max length of input
 
   /** Form model for the input. */
   comment = '';
@@ -47,12 +57,15 @@ export class InlineEditComponent {
   onSubmit() {
     if (this.popover) {
       this.popover.close(this.comment);
+      this.valueChange.emit(true)
     }
   }
-
+  
   onCancel() {
     if (this.popover) {
       this.popover.close();
+      this.comment = this._value // resets comment to original value
+      this.valueChange.emit(false)
     }
   }
 }
